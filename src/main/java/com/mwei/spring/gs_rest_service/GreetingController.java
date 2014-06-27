269ff3ef5365882a09b8b6ee30718df7669ec565
Michael Wei <michael.wei@Michaels-Mac-mini.local>
2014-06-26 21:03:23 -0400

    spring framework

---
+++ src/main/java/com/mwei/spring/gs_rest_service/GreetingController.java
@@ -0,0 +1,13 @@
+package com.mwei.spring.gs_rest_service;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+}
